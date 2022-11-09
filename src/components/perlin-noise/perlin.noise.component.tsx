913d1b12c8e082bf056e21d9eb75799bc30a9391
Peter Lobo <akumaokami10@gmail.com>
2022-11-08 23:35:12 -0500

    Update perlin.noise.component.tsx
    working in color change animation

--- src/components/perlin-noise/perlin.noise.component.tsx
+++ src/components/perlin-noise/perlin.noise.component.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Sketch from "react-p5";
 import p5Types from "p5";
 import "./perlin.noise.component.css"
@@ -9,11 +9,12 @@ const MAIN_ANGLE = 125;
 const SCALE = 0.8;
 
 export const PerlinNoiseComponent = (props: any) => {
-
     const [vertexHeight, setVertexHeight] = useState(300);
     const [vertexQuantity, setVertexQuantity] = useState(20);
     const [speed, setSpeed] = useState(0.01)
     const [colors, setColors] = useState(false)
+
+    const [terrainY, setTerrainY] = useState<terrainContainer[]>([]);
 
     let CANVAS_SIZE = {
         width: 1250,
@@ -23,6 +24,10 @@ export const PerlinNoiseComponent = (props: any) => {
     interface terrainComposition {
         terrainValue: number,
         color: any
+    }
+
+    interface terrainContainer {
+        terrain: terrainComposition[][]
     }
 
     let noiseScale = 6 / 150;
@@ -56,22 +61,78 @@ export const PerlinNoiseComponent = (props: any) => {
         return Math.abs(origin - 110)
     }
 
+    const preload = async (p5: p5Types) => {
+        await generateTerrain(p5);
+        addColors();
+    }
+
     const setup = (p5: p5Types, canvasParentRef: Element) => {
         p5.createCanvas(CANVAS_SIZE.width, 400, p5.WEBGL).parent(canvasParentRef);
         p5.angleMode(p5.DEGREES);
         onResize(p5);
         p5.noiseDetail(5, 0.5);
         p5.frameRate(60);
-        addColors();
     };
+
+    function delay(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    const generateTerrain = async (p5: p5Types) => {
+        let counter = 0
+        setInterval(() => {
+            fly -= speed
+            let yOff = fly
+            let terrainXTemp: terrainComposition[] = [];
+            let terrainYTemp: terrainComposition[][] = [];
+            for (let y = 0; y < ROWS; y++) {
+                let xOff = 0
+                for (let x = 0; x < COLS; x++) {
+                    let vertex = p5.map(p5.noise(xOff, yOff), 0, 1, -vertexHeight, vertexHeight)
+                    let terrain: terrainComposition = {
+                        terrainValue: 0,
+                        color: null
+                    }
+                    terrain.terrainValue = vertex
+                    if (counter > 100) {
+                        terrain.color = pickColor(vertex, p5)
+                    }
+                    terrainXTemp.push(terrain)
+                    xOff += noiseScale
+                }
+                terrainYTemp.push(terrainXTemp)
+                terrainXTemp = []
+                yOff += noiseScale
+            }
+            let terrainCollectorTemp: any[] = terrainY
+            let terrainContainer: terrainContainer = {
+                terrain: terrainYTemp
+            }
+            terrainCollectorTemp.push(terrainContainer)
+            setTerrainY(terrainCollectorTemp)
+            counter++
+        }, 60)
+    }
+
+    const getTerrain = () => {
+        if (terrainY.length > 0) {
+            console.log("terrain loaded")
+            let terrainYTemp = terrainY
+            let terrain = {terrain: terrainYTemp[0].terrain}
+            terrainYTemp.shift();
+            setTerrainY(terrainYTemp)
+            console.log({...terrain})
+            return {...terrain};
+        }
+    }
 
     const addColors = () => {
         setTimeout(() => {
             setColors(true)
-        }, 5000)
+        }, 2000)
     }
 
-    const  mouseDragged = (p5: p5Types) => {
+    const mouseDragged = (p5: p5Types) => {
         //mouseValueX = p5.mouseY //disabled
     }
 
@@ -81,7 +142,7 @@ export const PerlinNoiseComponent = (props: any) => {
         if (h < -230) {
             c = ocean;
         } else if (h < -120) {
-                c = shore;
+            c = shore;
         } else if (h < -70) {
             if (p5.random() > p5.pow(h - 2, 2) * 100) {
                 c = shore;
@@ -123,55 +184,34 @@ export const PerlinNoiseComponent = (props: any) => {
         }
     }
 
-    const draw = (p5: p5Types) => {
-        let terrainX: terrainComposition[] = [];
-        let terrainY: terrainComposition[][] = [];
-        fly -= speed
-        let yOff = fly
-        p5.scale(mouseScale)
-        for (let y = 0; y < ROWS; y++) {
-            let xOff = 0
-            for (let x = 0; x < COLS; x++) {
-                let vertex = p5.map(p5.noise(xOff, yOff), 0, 1, -vertexHeight, vertexHeight)
-                let terrain: terrainComposition = {
-                    terrainValue: 0,
-                    color: null
-                }
-                terrain.terrainValue = vertex
-                if (colors) {
-                    terrain.color = pickColor(vertex, p5)
-                }
-                terrainX.push(terrain);
-                xOff += noiseScale
-            }
-            terrainY.push(terrainX)
-            terrainX = []
-            yOff += noiseScale
-        }
+    const draw = async (p5: p5Types) => {
+        const terrainSource: any = getTerrain();
+        if (terrainSource) {
+            p5.scale(mouseScale)
+            p5.background("#282A36");
+            p5.stroke(255)
+            p5.noFill()
+            p5.smooth();
+            p5.rotateX(mouseValueX);
+            p5.rotate(MAIN_ANGLE)
+            p5.translate(-p5.width / 2, -p5.height / 2 - 200)
 
-        p5.background("#282A36"); //""
-        p5.stroke(255)
-        p5.noFill()
-        p5.smooth();
-        p5.rotateX(mouseValueX);
-        p5.rotate(MAIN_ANGLE)
-        p5.translate(-p5.width / 2, -p5.height / 2 - 200)
-
-        for (let y = 0; y < (ROWS - 1); y++) {
-            p5.beginShape(p5.TRIANGLE_STRIP)
-            for (let x = 0; x < COLS; x++) {
-                if (colors) {
-                    p5.fill(terrainY[x][y].color)
+            for (let y = 0; y < (ROWS - 1); y++) {
+                p5.beginShape(p5.TRIANGLE_STRIP)
+                for (let x = 0; x < COLS; x++) {
+                    if (terrainSource.terrain[x][y].color !== null) {
+                        p5.fill(terrainSource.terrain[x][y].color)
+                    }
+                    p5.vertex(x * vertexQuantity, y * vertexQuantity, terrainSource.terrain[x][y].terrainValue);
+                    p5.vertex(x * vertexQuantity, (y + 1) * vertexQuantity, terrainSource.terrain[x][y + 1].terrainValue);
                 }
-                p5.vertex(x * vertexQuantity, y * vertexQuantity, terrainY[x][y].terrainValue);
-                p5.vertex(x * vertexQuantity, (y + 1) * vertexQuantity, terrainY[x][y + 1].terrainValue);
+                p5.endShape()
             }
-            p5.endShape()
         }
     };
 
     return <>
-        <Sketch setup={setup} draw={draw} mouseDragged={mouseDragged}></Sketch>
+        <Sketch setup={setup} draw={draw} mouseDragged={mouseDragged} preload={preload}></Sketch>
         <PerlinNoiseControlComponent
             vertexHeight={vertexHeight}
             vertexQuantity={quantityPipe(vertexQuantity)}
